1ca8df3e9241562f1d8565aabbdfcceca217d0ba
Atin108 <atin.singh@primussoft.com>
2020-11-16 20:45:43 +0530

    Updates

---
+++ precache-manifest.1beda74b27e3f81afc3aeb705215399b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb471529dc9b5cff2b5f7ebbf968422",
+    "url": "/relic-test/index.html"
+  },
+  {
+    "revision": "2b1a092657a7a4b7c400",
+    "url": "/relic-test/static/css/main.1359096b.chunk.css"
+  },
+  {
+    "revision": "bd756f5675894d65d133",
+    "url": "/relic-test/static/js/2.8da72ca4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/relic-test/static/js/2.8da72ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1a092657a7a4b7c400",
+    "url": "/relic-test/static/js/main.5b72dce8.chunk.js"
+  },
+  {
+    "revision": "55d077368aab9d513d16",
+    "url": "/relic-test/static/js/runtime-main.5b340ee9.js"
+  }
+]);
